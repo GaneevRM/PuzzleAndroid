b98ba7bf0237442b35cc7ee2fda8f2108602b9fa
GaneevRM <ganeevrm@yahoo.com>
2020-12-04 22:09:09 +0400

    Updated AccountsActivity.java

--- app/src/main/java/ganeevrm/com/puzzleandroid/AccountsActivity.java
+++ app/src/main/java/ganeevrm/com/puzzleandroid/AccountsActivity.java
@@ -1,5 +1,6 @@
 package ganeevrm.com.puzzleandroid;
 
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,10 +10,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,9 +23,8 @@ public class AccountsActivity extends AppCompatActivity {
     private DatabaseHelper databaseHelper;
     private SQLiteDatabase db;
     private Cursor userCursor;
-    private SimpleCursorAdapter userAdapter;
     private boolean click;
-    private int selectUser;
+    private long selectUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,7 +41,7 @@ public class AccountsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
                 click = true;
-                selectUser = position;
+                selectUser = id;
             }
         };
         userGrid.setOnItemClickListener(itemListener);
@@ -59,68 +57,84 @@ public class AccountsActivity extends AppCompatActivity {
         //Определяем, какие столбцы из курсора будут выводиться в ListView
         String[] headers = new String[] {DatabaseHelper.COLUMN_LOGIN, DatabaseHelper.COLUMN_PASSWORD};
         //Создаем адаптер, передаем в него курсор
-        userAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_expandable_list_item_2, userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
+        SimpleCursorAdapter userAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_expandable_list_item_2, userCursor, headers, new int[]{android.R.id.text1, android.R.id.text2}, 0);
         header.setText("Найдено элементов: " + userCursor.getCount());
         userGrid.setAdapter(userAdapter);
     }
 
-
-
     public void onDelete(View view){
         if(click){
-
+            if(selectUser == 1){
+                Toast.makeText(getApplicationContext(), "Администратора нельзя удалить", Toast.LENGTH_SHORT).show();
+            }else {
+                db.delete(DatabaseHelper.TABLE, "_id = ?", new String[]{String.valueOf(selectUser)});
+                Toast.makeText(getApplicationContext(), "Пользователь удалён", Toast.LENGTH_SHORT).show();
+                recreate();
+            }
         } else Toast.makeText(getApplicationContext(), "Выберите пользователя", Toast.LENGTH_SHORT).show();
 
     }
 
     public void onBlock(View view){
         if(click){
+            if(selectUser == 1){
+                Toast.makeText(getApplicationContext(), "Администратора нельзя заблокировать", Toast.LENGTH_SHORT).show();
+            }else {
+                ContentValues cv = new ContentValues();
+                cv.put(DatabaseHelper.COLUMN_BLOCK, 1);
+                db.update(DatabaseHelper.TABLE, cv, DatabaseHelper.COLUMN_ID + "=" + String.valueOf(selectUser), null);
+                Toast.makeText(getApplicationContext(), "Пользователь заблокирован", Toast.LENGTH_SHORT).show();
+                recreate();
+            }
 
         } else Toast.makeText(getApplicationContext(), "Выберите пользователя", Toast.LENGTH_SHORT).show();
 
     }
     public void onAdd(View view){
-        if(click){
+        //Получаем вид с файла prompt.xml, который применим для диалогового окна:
+        LayoutInflater li = LayoutInflater.from(this);
+        View promptsView = li.inflate(R.layout.prompt, null);
 
-            //Получаем вид с файла prompt.xml, который применим для диалогового окна:
-            LayoutInflater li = LayoutInflater.from(this);
-            View promptsView = li.inflate(R.layout.prompt, null);
+        //Создаем AlertDialog
+        AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
 
-            //Создаем AlertDialog
-            AlertDialog.Builder mDialogBuilder = new AlertDialog.Builder(this);
+        //Настраиваем prompt.xml для нашего AlertDialog:
+        mDialogBuilder.setView(promptsView);
 
-            //Настраиваем prompt.xml для нашего AlertDialog:
-            mDialogBuilder.setView(promptsView);
+        //Настраиваем отображение поля для ввода текста в открытом диалоге:
+        final EditText loginInput = promptsView.findViewById(R.id.input_login);
 
-            //Настраиваем отображение поля для ввода текста в открытом диалоге:
-            final EditText loginInput = (EditText) promptsView.findViewById(R.id.input_login);
+        final EditText passwordInput =promptsView.findViewById(R.id.input_password);
 
-            final EditText passwordInput = (EditText) promptsView.findViewById(R.id.input_password);
+        //Настраиваем сообщение в диалоговом окне:
+        mDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                if(loginInput.getText().length()!=0 && passwordInput.getText().length()!=0){
+                                    ContentValues cv = new ContentValues();
+                                    cv.put(DatabaseHelper.COLUMN_BLOCK, Boolean.FALSE);
+                                    cv.put(DatabaseHelper.COLUMN_LOGIN, loginInput.getText().toString());
+                                    cv.put(DatabaseHelper.COLUMN_PASSWORD, passwordInput.getText().toString());
+                                    db.insert(DatabaseHelper.TABLE, null, cv);
+                                    Toast.makeText(getApplicationContext(), "Пользователь добавлен", Toast.LENGTH_SHORT).show();
+                                    recreate();
+                                }else Toast.makeText(getApplicationContext(), "Данные не введены", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                .setNegativeButton("Отмена",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                            }
+                        });
 
+        //Создаем AlertDialog:
+        AlertDialog alertDialog = mDialogBuilder.create();
 
-            //Настраиваем сообщение в диалоговом окне:
-            mDialogBuilder
-                    .setCancelable(false)
-                    .setPositiveButton("OK",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
-                                    //Вводим текст и отображаем в строке ввода на основном экране:
-                                    Toast.makeText(getApplicationContext(), "Добавление в БД", Toast.LENGTH_SHORT).show();
-                                }
-                            })
-                    .setNegativeButton("Отмена",
-                            new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog,int id) {
-                                    dialog.cancel();
-                                }
-                            });
-
-            //Создаем AlertDialog:
-            AlertDialog alertDialog = mDialogBuilder.create();
-
-            //и отображаем его:
-            alertDialog.show();
-        } else Toast.makeText(getApplicationContext(), "Выберите пользователя", Toast.LENGTH_SHORT).show();
+        //и отображаем его:
+        alertDialog.show();
     }
 
     public void onDestroy(){
