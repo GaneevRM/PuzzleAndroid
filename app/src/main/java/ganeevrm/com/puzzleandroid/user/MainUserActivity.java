ebb90bfc742421af01efe1f3c1c4bad9f3627b8d
GaneevRM <ganeevrm@yahoo.com>
2020-12-07 22:30:30 +0400

    Added MainUserActivity

---
+++ app/src/main/java/ganeevrm/com/puzzleandroid/user/MainUserActivity.java
@@ -0,0 +1,15 @@
+package ganeevrm.com.puzzleandroid.user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ganeevrm.com.puzzleandroid.R;
+
+public class MainUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_user);
+    }
+}
