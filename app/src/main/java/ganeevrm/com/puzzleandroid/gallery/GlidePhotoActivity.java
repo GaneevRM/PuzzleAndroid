a2f77f044a71176c3832f8a37cd5c249f90403ad
GaneevRM <ganeevrm@yahoo.com>
2020-12-09 23:57:07 +0400

    Added GlidPhotoActivity

---
+++ app/src/main/java/ganeevrm/com/puzzleandroid/gallery/GlidePhotoActivity.java
@@ -0,0 +1,79 @@
+package ganeevrm.com.puzzleandroid.gallery;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.graphics.Palette;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
+
+import ganeevrm.com.puzzleandroid.R;
+
+public class GlidePhotoActivity extends AppCompatActivity {
+
+    public static final String EXTRA_SPACE_PHOTO = "GlidePhotoActivity.SPACE_PHOTO";
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glid_photo);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+        String glidePhoto = getIntent().getStringExtra(EXTRA_SPACE_PHOTO);
+
+        Glide.with(this)
+                .load(glidePhoto)
+                .asBitmap()
+                .listener(new RequestListener<String, Bitmap>() {
+
+                    @Override
+                    public boolean onException(Exception e, String model, Target<Bitmap> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Bitmap resource, String model, Target<Bitmap> target, boolean isFromMemoryCache, boolean isFirstResource) {
+
+                        onPalette(Palette.from(resource).generate());
+                        mImageView.setImageBitmap(resource);
+
+                        return false;
+                    }
+
+                    public void onPalette(Palette palette) {
+                        if (null != palette) {
+                            ViewGroup parent = (ViewGroup) mImageView.getParent().getParent();
+                            parent.setBackgroundColor(palette.getDarkVibrantColor(Color.GRAY));
+                        }
+                    }
+                })
+                .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                .into(mImageView);
+
+    }
+
+   /* private SimpleTarget target = new SimpleTarget<Bitmap>() {
+
+        @Override
+        public void onResourceReady(Bitmap bitmap, GlideAnimation glideAnimation) {
+
+           onPalette(Palette.from(bitmap).generate());
+           mImageView.setImageBitmap(bitmap);
+        }
+
+        public void onPalette(Palette palette) {
+            if (null != palette) {
+                ViewGroup parent = (ViewGroup) mImageView.getParent().getParent();
+                parent.setBackgroundColor(palette.getDarkVibrantColor(Color.GRAY));
+            }
+        }
+    };*/
+}
