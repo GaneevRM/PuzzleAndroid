7cf22a2c358a491598d9078b363b5830a3a048a9
GaneevRM <ganeevrm@yahoo.com>
2021-01-28 19:06:12 +0400

    Updated RegistrationActivity.java

--- app/src/main/java/ganeevrm/com/puzzleandroid/authorization/RegistrationActivity.java
+++ app/src/main/java/ganeevrm/com/puzzleandroid/authorization/RegistrationActivity.java
@@ -1,13 +1,18 @@
 package ganeevrm.com.puzzleandroid.authorization;
 
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import ganeevrm.com.puzzleandroid.DatabaseHelper;
@@ -34,6 +39,8 @@ public class RegistrationActivity extends AppCompatActivity {
         passField = findViewById(R.id.editTextTextPassword);
 
         databaseHelper = new DatabaseHelper(getApplicationContext());
+        //Открываем подключение к БД
+        db = databaseHelper.getReadableDatabase();
 
     }
 
@@ -42,30 +49,40 @@ public class RegistrationActivity extends AppCompatActivity {
     }
 
     public void onOk(View view) {
-        //Открываем подключение к БД
-        db = databaseHelper.getReadableDatabase();
         //Получаем данные из БД в виде курсора
         userCursor =  db.rawQuery("SELECT * FROM "+ DatabaseHelper.TABLE, null);
-        if(userCursor.moveToFirst()){
-            boolean duplicate = false;
-            do{
-                String login = userCursor.getString(2);
-                //Если дубликат есть, то выводим сообщение
-                if ((loginField.getText().toString().equals(login))) {
-                    Toast.makeText(getApplicationContext(), "Такой пользователь уже существует", Toast.LENGTH_SHORT).show();
-                    duplicate = true;
-                    break;
+
+        if ((loginField.getText().length()<4)){
+            Toast.makeText(getApplicationContext(), "Минимальная длина"+"\n"+ "логина 4 символа", Toast.LENGTH_SHORT).show();
+        } else if ((loginField.getText().length()>10)){
+            Toast.makeText(getApplicationContext(), "Максимальная длина"+"\n"+ "логина 10 символов", Toast.LENGTH_SHORT).show();
+        } else if ((passField.getText().length()<6)){
+            Toast.makeText(getApplicationContext(), "Минимальная длина" +"\n"+ "пароля 6 символов", Toast.LENGTH_SHORT).show();
+        }else if ((passField.getText().length()>8)){
+            Toast.makeText(getApplicationContext(), "Максимальная длина"+"\n"+ "пароля 8 символов", Toast.LENGTH_SHORT).show();
+        }else {
+
+            if (userCursor.moveToFirst()) {
+                boolean duplicate = false;
+                do {
+                    String login = userCursor.getString(2);
+                    //Если дубликат есть, то выводим сообщение
+                    if ((loginField.getText().toString().equals(login))) {
+                        Toast.makeText(getApplicationContext(), "Такой пользователь уже существует", Toast.LENGTH_SHORT).show();
+                        duplicate = true;
+                        break;
+                    }
                 }
-            }
-            while (userCursor.moveToNext());
-            if(!duplicate){
-                ContentValues cv = new ContentValues();
-                cv.put(DatabaseHelper.COLUMN_LOGIN, loginField.getText().toString());
-                cv.put(DatabaseHelper.COLUMN_PASSWORD, passField.getText().toString());
-                db.insert(DatabaseHelper.TABLE, null, cv);
-                Toast.makeText(getApplicationContext(), "Вы зарегистрированы", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(this, MainUserActivity.class);
-                startActivity(intent);
+                while (userCursor.moveToNext());
+                if (!duplicate) {
+                    ContentValues cv = new ContentValues();
+                    cv.put(DatabaseHelper.COLUMN_LOGIN, loginField.getText().toString());
+                    cv.put(DatabaseHelper.COLUMN_PASSWORD, passField.getText().toString());
+                    db.insert(DatabaseHelper.TABLE, null, cv);
+                    Toast.makeText(getApplicationContext(), "Вы зарегистрированы", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(this, MainUserActivity.class);
+                    startActivity(intent);
+                }
             }
         }
         userCursor.close();
@@ -73,9 +90,8 @@ public class RegistrationActivity extends AppCompatActivity {
 
     public void onDestroy(){
         super.onDestroy();
-        //Закрываем подключение и курсор
-        db.close();
-        userCursor.close();
+        //Закрываем подключение
+        if (db.isOpen()) db.close();
     }
 
 }
